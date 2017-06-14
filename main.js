a859a91896fedc9d1d2ec1c9c14975ec80b6d438
Stephanie Yacek <stephyacek@gmail.com>
2017-06-14 09:23:51 -0700

    Create array for titles. #1

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 /* eslint-disable no-unused-vars */
+
+var product = [
+  { image: 'images/photo1.jpg',
+    title: 'Something With Turquoise'
+  },
+  { image: 'images/photo2.jpg',
+    title: 'Risible Rollings'
+  },
+  {title: 'Serpentine and Sarcastic'
+  },
+  {title: 'Corpulent Claws'
+  },
+  {title: 'Scarlet Sartorial Sense'
+  },
+  {title: 'Fulsome Fur'
+  },
+  {title: 'Zealous Eater'
+  },
+  {title: 'Flaming Fuschia'
+  }
+]
