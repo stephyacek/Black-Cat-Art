2fd3c3cfd305d7e1fbc8af513d9e47be471b5c5a
Stephanie Yacek <stephyacek@gmail.com>
2017-06-14 14:13:39 -0700

    Made requested changes. #1

--- main.js
+++ main.js
@@ -1,52 +1,62 @@
 /* eslint-disable no-unused-vars */
 
-var product = [
-  { image: 'images/photo1.jpg',
+var products = [
+  {
+    image: 'images/turquoise.jpg',
     title: 'Something With Turquoise'
   },
-  { image: 'images/photo2.jpg',
+  {
+    image: 'images/rolling.jpg',
     title: 'Risible Rollings'
   },
-  { image: 'images/photo3.jpg',
+  {
+    image: 'images/serpentine.jpg',
     title: 'Serpentine and Sarcastic'
   },
-  { image: 'images/photo4.jpg',
+  {
+    image: 'images/claws.jpg',
     title: 'Corpulent Claws'
   },
-  { image: 'images/photo5.jpg',
+  {
+    image: 'images/scarlet.jpg',
     title: 'Scarlet Sartorial Sense'
   },
-  { image: 'images/photo6.jpg',
+  {
+    image: 'images/fulsome.jpg',
     title: 'Fulsome Fur'
   },
-  { image: 'images/photo7.jpg',
+  {
+    image: 'images/zealous.jpg',
     title: 'Zealous Eater'
   },
-  { image: 'images/photo8.jpg',
+  {
+    image: 'images/fuschia.jpg',
     title: 'Flaming Fuschia'
   }
 ]
 
 var $artworkSection = document.querySelector('.artwork')
 
-document.addEventListener('DOMContentLoaded', renderPhotos(product))
+document.addEventListener('DOMContentLoaded', function () {
+  renderPhotos(products)
+})
 
-function createEachProduct(product) {
-  var $span = document.createElement('span')
+function createEachProduct(products) {
+  var $product = document.createElement('span')
   var $productDescription = document.createElement('span')
   var $image = document.createElement('img')
   $productDescription.setAttribute('class', 'product-description')
-  $image.setAttribute('src', product.image)
-  $span.setAttribute('class', 'product')
-  $span.appendChild($image)
-  $span.appendChild($productDescription)
-  $productDescription.textContent = product.title
-  return $span
+  $image.setAttribute('src', products.image)
+  $product.setAttribute('class', 'product')
+  $product.appendChild($image)
+  $product.appendChild($productDescription)
+  $productDescription.textContent = products.title
+  return $product
 }
 
-function renderPhotos(product) {
-  for (var i = 0; i < product.length; i++) {
-    var eachProduct = createEachProduct(product[i])
-    $artworkSection.appendChild(eachProduct)
+function renderPhotos(products) {
+  for (var i = 0; i < products.length; i++) {
+    var $product = createEachProduct(products[i])
+    $artworkSection.appendChild($product)
   }
 }
