bd46a4d743af478b8eda3cf5105ad9aa8d8d679e
Stephanie Yacek <stephyacek@gmail.com>
2017-06-14 10:56:39 -0700

    Made functions create spans to hold products. #1

--- main.js
+++ main.js
@@ -26,3 +26,27 @@ var product = [
     title: 'Flaming Fuschia'
   }
 ]
+
+var $artworkSection = document.querySelector('.artwork')
+
+document.addEventListener('DOMContentLoaded', renderPhotos(product))
+
+function createEachProduct(product) {
+  var $span = document.createElement('span')
+  var $productDescription = document.createElement('span')
+  var $image = document.createElement('img')
+  $productDescription.setAttribute('class', 'product-description')
+  $image.setAttribute('src', product.image)
+  $span.setAttribute('class', 'product')
+  $span.appendChild($image)
+  $span.appendChild($productDescription)
+  $productDescription.textContent = product.title
+  return $span
+}
+
+function renderPhotos(product) {
+  for (var i = 0; i < product.length; i++) {
+    var eachProduct = createEachProduct(product[i])
+    $artworkSection.appendChild(eachProduct)
+  }
+}
